12d09be47e5f6c99fc7e1d5437491ad8c891e07a
Sunrise55g <Sunrise55g@gmail.com>
2025-03-19 07:52:58 +0000

    Change

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
+apt-get update && apt-get upgrade -y
+apt-get install -y mc net-tools curl ca-certificates
 
+# Установка Node.js через nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm use --lts
+npm install -g npm
+npm install -g nest-cli
+npm install -g pnpm
 
-# apt-get update
-# apt-get upgrade -y
-# apt-get install -y mc net-tools curl ca-certificates/
+# Установка docker-compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-
-# python
-# apt-get install -y python3 build-essential libssl-dev libffi-dev python3-dev python3-venv
-
-
-# docker
-# apt-get upgrade -y docker-compose
-
-
-# node
-# wget -q -O- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-
-# export NVM_DIR="/root/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# nvm install --lts
-# nvm use --lts
-
-# npm i -g --update npm
-# npm i -g nest-cli
-# npm i -g pnpm
+# Запуск docker-compose (убери, если хочешь вручную)
+docker-compose -f /workspaces/example-0-nest/docker-compose-dev.yml up -d
